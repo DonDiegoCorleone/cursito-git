c65894f3fc6d20b68d604cba6b76b2678b9d09bb
Diego <sonicelerizo@uma.es>
2021-03-06 16:48:09 +0100

    Meto codigo

---
+++ Proyecto/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+        saludoBucle();
+        despedida();
+    }
+
+
+    /**
+     * Muestra "Hola mundo." 5 veces
+     * de una forma bastante ineficiente.
+     */
+    public static void saludoBucle() {
+        System.out.println("Hola mundo.");
+        System.out.println("Hola mundo.");
+        System.out.println("Hola mundo.");
+        System.out.println("Hola mundo.");
+        System.out.println("Hola mundo.");
+    }
+
+    /**
+     * Muestra un mensaje que indica el
+     * final de la ejecución del programa.
+     */
+    private static void despedida() {
+        System.out.println("Fin del programa.");
+    }
+}
